11e1bd365a46073dae615d5b4f2b0abd7de953eb
adarye <adavidparra0412@gmail.com>
2021-07-27 22:28:41 -0500

    Adolfo

--- src/app/crm/profile/profile.component.ts
+++ src/app/crm/profile/profile.component.ts
@@ -11,17 +11,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
 
-  obj:Preferencia = new Preferencia();
-  step:number = 1
+  obj: Preferencia = new Preferencia();
+  step: number = 1
   form: FormGroup;
   constructor(private profileService: ProfileService) {
-    this.profileService.get().subscribe(res=>{
-      this.obj = res;
-      this.form.patchValue(this.obj);
-    })
-   }
-
-  ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       facebook: new FormControl('', [Validators.required]),
@@ -31,32 +24,49 @@ export class ProfileComponent implements OnInit {
       youtube: new FormControl(''),
       whatsapp: new FormControl('', [Validators.required]),
       name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
-      username: new FormControl('', [Validators.required,  Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
-      age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
+      age: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{1,2}$')]),
       residence: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
       address: new FormControl(''),
-      rol:  new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
-      about_me: new FormControl('', [Validators.required, Validators.minLength(40), Validators.maxLength(60)]),
+      rol: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]),
+      about_me: new FormControl('', [Validators.required, Validators.minLength(40), Validators.maxLength(600)]),
       // make a form control for a pdf file
-       cv: new FormControl('', [Validators.pattern('^.*\.pdf$')])
+      cv: new FormControl('', [Validators.pattern('^.*\.pdf$')])
 
     });
+    this.profileService.get().subscribe(res => {
+      if (res) {
+        this.obj = res;
+        this.form.patchValue(this.obj);
+      }
+      else {
+        this.form.reset();
+        this.form.markAsPristine();
+      }
+    })
+
+
   }
-  post(){
-     this.step = 1
+
+  ngOnInit(): void {
+
+
+  }
+  post() {
+    // this.step = 1
     console.log(this.form)
     this.obj = this.form.value;
-    if(this.form.valid){
-this.profileService.post(this.obj).subscribe(res=>{
-      // this.step = 2;
-    })
+    if (this.form.valid) {
+      this.profileService.post(this.obj).subscribe(res => {
+         this.step = 4;
+      })
     }
 
 
   }
   get about_me() {
     return this.form.get('about_me');
-}
+  }
 
 
 
